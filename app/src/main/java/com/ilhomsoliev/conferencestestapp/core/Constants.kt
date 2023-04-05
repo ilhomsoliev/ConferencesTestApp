b20b9d232da173f106f11ea91a8ef62248703ad4
ilhomsoliev <solievoliver@gmail.com>
2023-04-05 14:34:55 +0500

    paging logic added

--- app/src/main/java/com/ilhomsoliev/conferencestestapp/core/Constants.kt
+++ app/src/main/java/com/ilhomsoliev/conferencestestapp/core/Constants.kt
@@ -3,5 +3,5 @@ package com.ilhomsoliev.conferencestestapp.core
 object Constants {
     const val URL_ARG_ID = "URL_ARG_ID"
     const val BASE_URL = "https://guidebook.com/"
-
+    const val PAGE_SIZE = 3
 }
